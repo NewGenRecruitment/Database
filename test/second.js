43bfafffeeda4e587dea798892775c2ce9fc8572
Josh Cole <josh@thecole.co.uk>
2014-10-21 10:28:12 +0100

    Restored test for multiple connections code

--- test/second.js
+++ test/second.js
@@ -1,4 +1,4 @@
-var database = require('../database');
+var database = require('../database').use('main-db');
 
 module.exports.go = function () {
 
--- test/test.js
+++ test/test.js
@@ -1,11 +1,15 @@
-var database       = require('../database')
-  , DatabaseSchema = require('./schema.json')
-  , second         = require('./second');
+var databaseModule     = require('../database')
+  , DatabaseConnection = databaseModule.Connection
+  , DatabaseSchema     = require('./schema.json')
+  , second             = require('./second');
 
-database.connect({
-    schema:      DatabaseSchema
+var database = new DatabaseConnection({
+    dbId:        'main-db'
+  , schema:      DatabaseSchema
   , credentials: "mongodb://newgen-rec-dev-b1c974d0:f23022eb537af3e5d87c1ec8e43656ba@ds053109.mongolab.com:53109/newgen-crm-dev"
-}, function (err) {
+});
+
+database.connect(function (err) {
 
   if (err) {
     console.log('Database: Failed to connect!');
